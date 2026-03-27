f9c9fcbec7361a36f09cc78c9cbe57c2d6c581dc
Aicent Stack <lee@aicent.com>
2026-03-27 17:21:34 +0800

    Create lib.rs

---
+++ src/lib.rs
@@ -0,0 +1,8 @@
+//! Aicent - The Brain of Aicent Stack
+//! AID Identity management + Task primitive decomposition + High-level cognitive scheduling
+
+// TODO: Dependencies such as RTTP and RPKI will be introduced here later.
+
+pub mod brain;
+
+pub use brain::*;
